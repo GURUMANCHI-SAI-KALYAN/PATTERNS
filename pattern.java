0e3b5109bc53d4fa1cd2144f0664927656bbf08b
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-13 17:56:03 +0530

    ADDED : NEW REPO AND PINCODE PROGRAM

---
+++ pattern.java
@@ -0,0 +1,10 @@
+package samplePackages;
+import java.util.regex.*;
+
+  public class pattern {
+	public static void main(String[] args) {
+
+		System.out.println(Pattern.matches("[1-9][0-9]{5}", "400084"));
+	}
+}
+
