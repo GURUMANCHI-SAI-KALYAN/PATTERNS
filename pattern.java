9448d3797790ac3ea2d650fb43ec4418432fa19c
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-13 19:45:14 +0530

    ADDED : Check last word is not alphabet or char program

--- pattern.java
+++ pattern.java
@@ -4,7 +4,9 @@ import java.util.regex.*;
   public class pattern {
 	public static void main(String[] args) {
 
-		System.out.println(Pattern.matches("![a-zA-Z][1-9][0-9]{5}", "A00084"));
+		System.out.println(Pattern.matches("^[0-9]{5}[^$*#a-zA-Z]*$", "500085"));
+//		 System.out.println(Pattern.matches("[^-]{1}", "8"));
+
 	}
 }
 
